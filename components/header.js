2f91e73bffaa4f41769b930e20f5689429666135
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2022-12-11 19:54:40 -0300

    the header content color changed: text-white removed

--- components/header.js
+++ components/header.js
@@ -9,7 +9,7 @@ const Header = ({ categories }) => {
           <ul className="">
             <li>
               <Link href="/">
-                <a className="cursor-pointer font-bold text-4xl text-white">
+                <a className="cursor-pointer font-bold text-4xl .white-mode">
                   Hamilton Dev
                 </a>
               </Link>
@@ -22,7 +22,7 @@ const Header = ({ categories }) => {
               return (
                 <li key={category.id}>
                   <Link href={`/category/${category.attributes.slug}`}>
-                    <a className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+                    <a className="md:float-right mt-2 align-middle .white-mode ml-4 font-semibold cursor-pointer">
                       {category.attributes.name}
                     </a>
                   </Link>
