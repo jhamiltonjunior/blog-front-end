0ae7d2a5314ba6d22fb0900b155b4914c416e8c8
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2022-12-11 21:49:42 -0300

    test

---
+++ pages/index.js
@@ -0,0 +1,50 @@
+import React from "react"
+// import { FeaturedPosts } from "../sections/index"
+import Articles from "../components/articles"
+import Layout from "../components/layout"
+import Categories from "../components/categories"
+import Seo from "../components/seo"
+import { fetchAPI } from "../lib/api"
+import Header from "../components/header"
+
+const Home = ({ articles, categories, homepage }) => {
+  return (
+    <Layout categories={categories}>
+      <Seo seo={homepage.attributes.seo} />
+      {/* className="grid grid-cols-1 lg:grid-cols-12 gap-12" */}
+      {/* <h1>{homepage.attributes.hero.title}</h1> */}
+      <Articles articles={articles} />
+
+      <div className="lg:col-span-4 col-span-2">
+        <div className="lg:sticky relative top-8">
+          <Categories categories={categories} />
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export async function getStaticProps() {
+  // Run API calls in parallel
+  const [articlesRes, categoriesRes, homepageRes] = await Promise.all([
+    fetchAPI("/articles", { populate: "*" }),
+    fetchAPI("/categories", { populate: "*" }),
+    fetchAPI("/homepage", {
+      populate: {
+        hero: "*",
+        seo: { populate: "*" },
+      },
+    }),
+  ])
+
+  return {
+    props: {
+      articles: articlesRes.data,
+      categories: categoriesRes.data,
+      homepage: homepageRes.data,
+    },
+    revalidate: 1,
+  }
+}
+
+export default Home
