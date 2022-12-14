f8a0a9912d6284236324364d31cfb017a5870425
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2022-12-13 23:58:25 -0300

    size text of nav header increased

--- components/header.js
+++ components/header.js
@@ -21,7 +21,7 @@ const Header = ({ categories }) => {
           className="md:flex items-center space-x-1"
         >
           <ul
-            className="flex p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+            className="flex p-4 mt-4 border border-gray-100 rounded-lg text-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
             // flex mt-4 font-medium justify-between lg:flex-row lg:space-x-8 lg:mt-0 hidden w-full md:block md:w-auto
           >
             {categories.map((category) => {
