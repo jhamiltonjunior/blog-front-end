a0c8052d1436b65cd5cb827b91898be555961a58
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2022-12-13 21:40:55 -0300

    new style apply to header

--- components/header.js
+++ components/header.js
@@ -3,10 +3,10 @@ import Link from "next/link"
 
 const Header = ({ categories }) => {
   return (
-    <header className="container mx-auto px-10 mb-8">
-      <nav className="border-b w-full inline-block border-blue-400 py-8">
-        <div className="md:float-left block">
-          <ul className="">
+    <header className="bg-white dark:bg-gray-800 border-gray-200 lg:px-6 mx-auto py-2 px-10 mb-8">
+      <nav className="flex flex-wrap justify-between items-center mx-auto">
+        <div className="block">
+          <ul>
             <li>
               <Link href="/">
                 <a className="cursor-pointer font-bold text-4xl .white-mode">
@@ -16,13 +16,16 @@ const Header = ({ categories }) => {
             </li>
           </ul>
         </div>
-        <div className="hidden md:float-left md:contents">
-          <ul>
+        <div
+          className="flex cursor-pointer justify-between items-center lg:flex lg:w-auto lg:order-1"
+          id="mobile-menu-2"
+        >
+          <ul className="flex mt-4 font-medium justify-between lg:flex-row lg:space-x-8 lg:mt-0">
             {categories.map((category) => {
               return (
                 <li key={category.id}>
                   <Link href={`/category/${category.attributes.slug}`}>
-                    <a className="md:float-right mt-2 align-middle .white-mode ml-4 font-semibold cursor-pointer">
+                    <a className="block py-2 pr-4 pl-3  rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white">
                       {category.attributes.name}
                     </a>
                   </Link>
