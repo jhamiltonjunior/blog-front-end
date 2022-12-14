36fc7a01bce6e914c93df19a6d65713760661566
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2022-12-13 22:20:27 -0300

    more styles added in header

--- components/header.js
+++ components/header.js
@@ -4,7 +4,7 @@ import Link from "next/link"
 const Header = ({ categories }) => {
   return (
     <header className="bg-white dark:bg-gray-800 border-gray-200 lg:px-6 mx-auto py-2 px-10 mb-8">
-      <nav className="flex flex-wrap justify-between items-center mx-auto">
+      <nav className="2xl:container flex flex-wrap justify-between items-center mx-auto">
         <div className="block">
           <ul>
             <li>
@@ -17,15 +17,18 @@ const Header = ({ categories }) => {
           </ul>
         </div>
         <div
-          className="flex cursor-pointer justify-between items-center lg:flex lg:w-auto lg:order-1"
-          id="mobile-menu-2"
+          // className="flex cursor-pointer justify-between items-center lg:flex lg:w-auto lg:order-1"
+          className="md:flex items-center space-x-1"
         >
-          <ul className="flex mt-4 font-medium justify-between lg:flex-row lg:space-x-8 lg:mt-0">
+          <ul
+            className="flex p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+            // flex mt-4 font-medium justify-between lg:flex-row lg:space-x-8 lg:mt-0 hidden w-full md:block md:w-auto
+          >
             {categories.map((category) => {
               return (
                 <li key={category.id}>
                   <Link href={`/category/${category.attributes.slug}`}>
-                    <a className="block py-2 pr-4 pl-3  rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white">
+                    <a className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
                       {category.attributes.name}
                     </a>
                   </Link>
