7cb7a0b81be86f73c018f223f39a1a67902afaf5
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2022-12-13 22:48:55 -0300

    link changed for wrap all card

--- components/card.js
+++ components/card.js
@@ -7,60 +7,59 @@ import NextImage from "./image"
 const Card = ({ article }) => {
   return (
     <div className="w-auto bg-white shadow-lg rounded-lg p-0 pb-12 mb-3">
-      <div className="relative overflow-hidden text-center mb-6 rounded-t-lg w-full object-cover">
-        <NextImage image={article.attributes.image} />
-      </div>
+      <Link href={`/article/${article.attributes.slug}`}>
+        <a>
+          <div className="relative overflow-hidden text-center mb-6 rounded-t-lg w-full object-cover">
+            <NextImage image={article.attributes.image} />
+          </div>
 
-      <Link href={`/article/${article.attributes.slug}`} passHref>
-        <h2 className="transition duration-700 text-center mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold">
-          {article.attributes.title}
-        </h2>
-      </Link>
+          <h2 className="transition duration-700 text-center mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold">
+            {article.attributes.title}
+          </h2>
 
-      {/* {console.log(article.attributes.title)} */}
+          {/* {console.log(article.attributes.title)} */}
 
-      <div className="text-center font-medium text-gray-700">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6 inline mr-2 text-pink-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-          />
-        </svg>
-        <span className="align-middle">
-          <Moment format="MMM DD YYYY">
-            {article.attributes.published_at}
-          </Moment>
-        </span>
-      </div>
+          <div className="text-center font-medium text-gray-700">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 inline mr-2 text-pink-500"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+              />
+            </svg>
+            <span className="align-middle">
+              <Moment format="MMM DD YYYY">
+                {article.attributes.published_at}
+              </Moment>
+            </span>
+          </div>
 
-      <p className="text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8 md:truncate">
-        {article.attributes.description}
-      </p>
-      <div className="text-center">
-        <Link href={`/article/${article.attributes.slug}`} passHref>
-          <span className="transition duration-500 ease transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">
-            Continue Reading
-          </span>
-        </Link>
-      </div>
+          <p className="text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8 md:truncate">
+            {/* {article.attributes.description} */}
+          </p>
+          <div className="text-center">
+            <span className="transition duration-500 ease transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">
+              Continue Reading
+            </span>
+          </div>
 
-      <div className="uk-cardbody">
-        <p id="category" className="uk-text-uppercase">
-          {article.attributes.category.name}
-        </p>
-        {/* {console.log(article.attributes)} */}
-        {/* <p id="title" className="uk-text-large">
+          <div className="">
+            <p id="category" className="">
+              {/* {article.attributes.category.data.attributes.name} */}
+            </p>
+            {/* <p id="title" className="uk-text-large">
               {article.attributes.title}
             </p> */}
-      </div>
+          </div>
+        </a>
+      </Link>
     </div>
   )
 }
