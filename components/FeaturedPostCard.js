8a21bf396e28fa89971033a25f73f7dbd8c219d2
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2022-12-14 22:07:11 -0300

    ; removed

--- components/FeaturedPostCard.js
+++ components/FeaturedPostCard.js
@@ -1,6 +1,6 @@
 import React from "react"
 import moment from "moment"
-import Image from "next/legacy/image";
+import Image from "next/legacy/image"
 import Link from "next/link"
 
 const FeaturedPostCard = ({ post }) => (
