c933b83c39cc95b99c32acf42352420542c7f3ed
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2022-12-14 22:01:34 -0300

    next-image-to-legacy-image added in files

--- components/card.js
+++ components/card.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Link from "next/link"
 import Moment from "react-moment"
-import Image from "next/image"
+import Image from "next/legacy/image"
 import NextImage from "./image"
 
 const Card = ({ article }) => {
