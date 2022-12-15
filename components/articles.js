03427799559fa4e0ea44c9cc04aa13f8132d7017
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2022-12-14 22:48:20 -0300

    logic implemented to get all articles

--- components/articles.js
+++ components/articles.js
@@ -3,22 +3,11 @@ import Card from "./card"
 import Pagination from "./pagination"
 
 const Articles = ({ articles }) => {
-  const leftArticlesCount = Math.ceil(articles.length / 5)
-  const leftArticles = articles.slice(0, leftArticlesCount)
-  const rightArticles = articles.slice(leftArticlesCount, articles.length)
-
   return (
     <div>
       <div className="grid gap-10 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
-        {leftArticles.map((article, i) => {
-          return (
-            <Card
-              article={article}
-              key={`article__left__${article.attributes.slug}`}
-            />
-          )
-        })}
-        {rightArticles.map((article, i) => {
+        {console.log(articles)}
+        {articles.map((article, i) => {
           return (
             <Card
               article={article}
