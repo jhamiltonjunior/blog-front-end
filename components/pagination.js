a053662ad4b747b6c34e1cc17944284421a6d219
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2022-12-17 20:17:13 -0300

    style: more style added in button of pagintion

--- components/pagination.js
+++ components/pagination.js
@@ -8,17 +8,19 @@ const Pagination = ({ page, articles }) => {
   return (
     <div className="flex items-center justify-center">
       <nav
-        className="my-10 isolate inline-flex -space-x-px rounded-md shadow-sm"
+        className="w-80 my-10 isolate flex items-center justify-between -space-x-px rounded-md shadow-sm"
         aria-label="Pagination"
       >
         <button
           onClick={() => router.push(`/?page=${page - 1}`)}
           disabled={page <= 1}
-          className="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20"
+          className={`relative rounded-full inline-flex items-center rounded-l-md border border-gray-300 bg-white px-3 py-3 text-sm font-medium text-black-500 hover:bg-gray-50 focus:z-20 ${
+            page <= 1 ? "hidden" : ""
+          }`}
         >
           <svg
             aria-hidden="true"
-            className="w-5 h-5 mr-2"
+            className="w-6 h-6"
             fill="currentColor"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
@@ -29,18 +31,18 @@ const Pagination = ({ page, articles }) => {
               clipRule="evenodd"
             ></path>
           </svg>
-          <span>Prev</span>
         </button>
-
+        <span>Página 1 de 3</span>
         <button
           onClick={() => router.push(`/?page=${page + 1}`)}
           disabled={page >= lastPage}
-          className="relative inline-flex items-center rounded-r-md border border-gray-300 text-gray-500 bg-white px-2 py-2 text-sm font-medium  hover:bg-gray-50 focus:z-20"
+          className={`relative rounded-full inline-flex items-center border border-gray-300 text-black-500 bg-white px-3 py-3 text-sm font-medium  hover:bg-gray-50 focus:z-20 ${
+            page >= lastPage ? "hidden" : ""
+          }`}
         >
-          <span>Next</span>
           <svg
             aria-hidden="true"
-            className="w-5 h-5 ml-2"
+            className="w-6 h-6"
             fill="currentColor"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
