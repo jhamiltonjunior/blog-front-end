39bf99d781f4bd8656205b0d38ba89305c0c71e0
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2022-12-19 18:12:00 -0300

    accebility: default lang pt-BR added

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+  i18n: {
+    locales: ["pt-BR"],
+    defaultLocale: "pt-BR",
+  },
   images: {
     loader: "default",
     domains: ["localhost", "127.0.0.1", "blog.hamiltonjr.dev"],
