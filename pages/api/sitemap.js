db0f79fcc12a16103ec1f5ecdcd757290074a5f0
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2022-12-19 12:29:46 -0300

    fix: sitemap does not have "article/" in your ulrs

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -3,17 +3,29 @@ const { Readable } = require("stream")
 
 export default async function sitemap(req, res) {
   const links = [
-    { url: "what-s-inside-a-black-hole", changeFreq: "daily", priority: 0.3 },
-    { url: "the-internet-s-own-boy", changeFreq: "daily", priority: 0.3 },
-    { url: "this-shrimp-is-awesome", changeFreq: "daily", priority: 0.3 },
     {
-      url: "a-bug-is-becoming-a-meme-on-the-internet",
+      url: "article/what-s-inside-a-black-hole",
       changeFreq: "daily",
       priority: 0.3,
     },
-    { url: "we-love-pizza", changeFreq: "daily", priority: 0.3 },
-    { url: "beautiful-picture", changeFreq: "daily", priority: 0.3 },
-    { url: "i-testing", changeFreq: "daily", priority: 0.3 },
+    {
+      url: "article/the-internet-s-own-boy",
+      changeFreq: "daily",
+      priority: 0.3,
+    },
+    {
+      url: "article/this-shrimp-is-awesome",
+      changeFreq: "daily",
+      priority: 0.3,
+    },
+    {
+      url: "article/a-bug-is-becoming-a-meme-on-the-internet",
+      changeFreq: "daily",
+      priority: 0.3,
+    },
+    { url: "article/we-love-pizza", changeFreq: "daily", priority: 0.3 },
+    { url: "article/beautiful-picture", changeFreq: "daily", priority: 0.3 },
+    { url: "article/i-testing", changeFreq: "daily", priority: 0.3 },
   ]
 
   const stream = new SitemapStream({ hostname: `https://${req.headers.host}` })
