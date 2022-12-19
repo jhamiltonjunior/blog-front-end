f3f88baf1f035d4cbd648454d348d6c91edf5012
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2022-12-19 19:48:29 -0300

    text of page 404 altered and tag a altered to Link next component

--- pages/404.js
+++ pages/404.js
@@ -26,15 +26,15 @@ const notFound = ({ categories }) => {
           </h6>
 
           <p className="mb-8 text-center text-gray-500 md:text-lg">
-            The page you’re looking for doesn’t exist.
+            A página que você está acessando não existe.
           </p>
 
-          <a
-            href="#"
+          <Link
+            href="/"
             className="px-6 py-2 text-sm font-semibold text-blue-800 bg-blue-100"
           >
-            Go home
-          </a>
+            Home
+          </Link>
         </div>
       </div>
     </div>
