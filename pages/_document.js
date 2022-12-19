75f6af1d161169c1588d2eb5935f6b3639d8d951
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2022-12-19 19:14:38 -0300

    script to google analitycs added

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,27 @@
 import Document, { Html, Head, Main, NextScript } from "next/document"
+import Script from "next/script"
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
+          <Script
+            strategy="lazyOnload"
+            src={`https://www.googletagmanager.com/gtag/js?id=G-HL9GELG1VR`}
+          />
+
+          <Script strategy="lazyOnload">
+            {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-HL9GELG1VR', {
+        page_path: window.location.pathname,
+        });
+    `}
+          </Script>
+
           {/* pinterest verification */}
           <meta
             name="p:domain_verify"
