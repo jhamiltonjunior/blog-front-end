880fdec769c042804a994a84f019e28f9f50e6d1
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2022-12-20 23:27:39 -0300

    categoria nav: added hover color hex 695AA6

--- components/header.js
+++ components/header.js
@@ -29,7 +29,7 @@ const Header = ({ categories }) => {
               return (
                 <li key={category.id}>
                   <Link href={`/category/${category.attributes.slug}`} passHref>
-                    <div className="py-2 pl-3 pr-4 text-black rounded hidden md:block hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                    <div className="py-2 pl-3 pr-4 text-black rounded hidden md:block hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-[#695AA6] md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
                       {category.attributes.name}
                     </div>
                   </Link>
