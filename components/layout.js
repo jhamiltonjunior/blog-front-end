decd45530d305ce372fac084e6f0b0fa23c62f22
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2022-12-20 23:13:17 -0300

    footer template added

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,11 @@
+import Footer from "./footer"
 import Header from "./header"
 
 const Layout = ({ children, categories, seo }) => (
   <>
     <Header categories={categories} />
     <main className="mx-auto px-4 max-w-screen-xl">{children}</main>
+    <Footer />
   </>
 )
 
