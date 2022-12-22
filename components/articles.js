0fd41def145be72087cd69b6bcf819ef9ec7ad0d
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2022-12-22 19:55:24 -0300

    margin top added in card articles

--- components/articles.js
+++ components/articles.js
@@ -5,7 +5,7 @@ import Pagination from "./pagination"
 const Articles = ({ articles }) => {
   return (
     <div>
-      <div className="grid gap-10 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
+      <div className="grid gap-10 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 mt-2">
         {articles.map((article, i) => {
           return <Card article={article} key={article.attributes.slug} />
         })}
