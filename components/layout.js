fa8d78c57b609eeceb223f079e9777190d0fe73f
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2023-01-20 22:48:01 -0300

    min heigth screen changed to full view screen

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,9 @@ import Header from "./header"
 const Layout = ({ children, categories, seo }) => (
   <div className="dark:bg-[#111111]">
     <Header categories={categories} />
-    <main className="mx-auto px-4 max-w-screen-xl">{children}</main>
+    <main className="mx-auto px-4 max-w-screen-xl min-h-screen">
+      {children}
+    </main>
     <Footer />
   </div>
 )
